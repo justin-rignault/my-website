864e69fe0376ea7f46e4cfbf30a38895a1364b3d
justin-rignault <justinrignault@gmail.com>
2020-08-21 15:27:24 -0300

    restarted and completed navbar

--- js/code.js
+++ js/code.js
@@ -6,23 +6,8 @@ Description: main javascript file linked to index.html for my personal website
 'use strict'
 
 function init() {
-    setNavbar()
     setFavicon()
     addEventListeners()
-}
-
-const setNavbar = () => {
-    const mb = isMobileBrowser()
-    const desktopNav = document.getElementById('desktop-nav')
-    const mobileNav = document.getElementById('mobile-nav')
-    const navHidden = desktopNav.classList.value.includes('hidden') && mobileNav.classList.value.includes('hidden')
-
-    if(mb && navHidden) {
-        show(mobileNav)
-    } else if(!mb && navHidden) {
-        show(desktopNav)
-    }
-
 }
 
 const setFavicon = () => {
@@ -40,26 +25,6 @@ const setFavicon = () => {
 }
 
 const addEventListeners = () => {
-    document.getElementById('hamburger-menu').addEventListener('click', showMenu)
-    document.querySelector('.x-icon').addEventListener('click', closeMenu)
-    document.querySelectorAll('.mobile-nav-li').forEach( link => link.addEventListener('click', closeMenu) )
-}
-
-const showMenu = () => {
-    const navbar = document.getElementById('mobile-nav')
-    const mobileMenu = document.getElementById('mobile-menu')
-
-    hide(navbar)
-    show(mobileMenu)
-    
-}
-
-const closeMenu = () => {
-    const navbar = document.getElementById('mobile-nav')
-    const mobileMenu = document.getElementById('mobile-menu')
-
-    show(navbar)
-    hide(mobileMenu)
 }
 
 const hide = elem => {
